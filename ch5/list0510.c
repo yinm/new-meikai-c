de601486110b5245b4bed9c7088acf344ad123ec
yinm <yinm.cminor9@gmail.com>
2018-01-30 21:07:49 +0900

    Check macro

---
+++ ch5/list0510.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define NUMBER 5
+
+int main(void)
+{
+    int i;
+    int tensu[NUMBER];
+    int sum = 0;
+
+    printf("%d人の点数を入力してください。\n", NUMBER);
+    for (i = 0; i < NUMBER; i++) {
+        printf("%2d番:", i + 1);
+        scanf("%d", &tensu[i]);
+        sum += tensu[i];
+    }
+
+    printf("合計点:%5d\n", sum);
+    printf("平均点:%5.1f\n", (double)sum / NUMBER);
+
+    return 0;
+}
