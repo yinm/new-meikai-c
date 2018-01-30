e256a2875537c7160fc4a479d67b33f5eeccc03b
yinm <yinm.cminor9@gmail.com>
2018-01-30 09:19:04 +0900

    Check break

---
+++ ch4/list0416a.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int i, j;
+
+  for (i = 1; i <= 9; i++) {
+    for (j = 1; j <= 9; j++) {
+      int seki = i * j;
+      if (seki > 40)
+        break;
+      printf("%3d", seki);
+    }
+    putchar('\n');
+  }
+
+  return 0;
+}
