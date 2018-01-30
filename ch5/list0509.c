d8d30a043668b1cc9b0dd94db047ebe6779edffa
yinm <yinm.cminor9@gmail.com>
2018-01-30 21:01:13 +0900

    Check array

---
+++ ch5/list0509.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+    int tensu[5];
+    int sum = 0;
+
+    printf("5人の点数を入力してください。\n");
+    for (i = 0; i < 5; i++) {
+        printf("%2d番:" , i + 1);
+        scanf("%d", &tensu[i]);
+        sum += tensu[i];
+    }
+
+    printf("合計点:%5d\n", sum);
+    printf("平均点:%5.1f\n", (double)sum / 5);
+
+    return 0;
+}
