6979e4c748b28da250767da73667228cccce9f1c
yinm <yinm.cminor9@gmail.com>
2018-02-05 08:15:42 +0900

    Check enum

---
+++ ch8/list0806.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+enum animal { Dog, Cat, Monkey, Invalid };
+
+void dog(void)
+{
+    puts("ワンワン!!");
+}
+
+void cat(void)
+{
+    puts("にゃー!!");
+}
+
+void monkey(void)
+{
+    puts("キッキッ!!");
+}
+
+enum animal select(void)
+{
+    int tmp;
+
+    do {
+        printf("0:犬 1:猫 2:猿 3:終了 :");
+        scanf("%d", &tmp);
+    } while (tmp < Dog || tmp > Invalid);
+
+    return tmp;
+}
+
+int main(void)
+{
+    enum animal selected;
+
+    do {
+        switch (selected = select()) {
+            case Dog:
+                dog();
+                break;
+            case Cat:
+                cat();
+                break;
+            case Monkey:
+                monkey();
+                break;
+        }
+    } while (selected != Invalid);
+
+    return 0;
+}
