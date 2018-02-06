4802094af1392b5f678e8730ff62e0e5976bf2b0
yinm <yinm.cminor9@gmail.com>
2018-02-06 09:13:36 +0900

    Check char array

---
+++ ch9/list0902.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char str[4];
+
+    str[0] = 'A';
+    str[1] = 'B';
+    str[2] = 'C';
+    str[3] = '\0';
+
+    printf("文字列strは\"%s\"です。\n", str);
+
+    return 0;
+}
