858d8da86821f2413584ec266658488d00f64529
yinm <yinm.cminor9@gmail.com>
2018-03-02 22:35:04 +0900

    Check 2nd

--- ch12/list1207.c
+++ ch12/list1207.c
@@ -1,3 +1,4 @@
+// 2nd
 #include <stdio.h>
 #include <string.h>
 
@@ -23,7 +24,7 @@ void sort_by_height(Student a[], int n)
     int i, j;
 
     for (i = 0; i < n - 1; i++) {
-        for (j = n -1; j > i; j--) {
+        for (j = n - 1; j > i; j--) {
             if (a[j - 1].height > a[j].height) {
                 swap_Student(&a[j - 1], &a[j]);
             }
@@ -35,22 +36,28 @@ int main(void)
 {
     int i;
     Student std[] = {
-        { "Sato", 178, 61.2, 80000 },
-        { "Sanaka", 175, 62.5, 730000 },
-        { "Takao", 173, 86.2, 0 },
-        { "Mike", 165, 72.3, 700000 },
-        { "Masaki", 179, 77.5, 70000 },
+        { "a", 178, 61.2, 80000 },
+        { "b", 175, 62.5, 73000 },
+        { "c", 173, 86.2, 0 },
+        { "d", 165, 72.3, 70000 },
+        { "e", 179, 77.5, 70000 },
     };
 
     for (i = 0; i < NUMBER; i++) {
-        printf("%-8s %6d%6.1f%7ld\n", std[i].name, std[i].height, std[i].weight, std[i].schols);
+        printf(
+            "%-8s %6d%6.1f%7ld\n",
+            std[i].name, std[i].height, std[i].weight, std[i].schols
+        );
     }
 
     sort_by_height(std, NUMBER);
 
     puts("\n身長順にソートしました。");
     for (i = 0; i < NUMBER; i++) {
-        printf("%-8s %6d%6.1f%7ld\n", std[i].name, std[i].height, std[i].weight, std[i].schols);
+        printf(
+            "%-8s %6d%6.1f%7ld\n",
+            std[i].name, std[i].height, std[i].weight, std[i].schols
+        );
     }
 
     return 0;
