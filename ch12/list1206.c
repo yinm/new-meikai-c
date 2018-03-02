bf1c511a72eb0d58e771c6179812762a5473fc13
yinm <yinm.cminor9@gmail.com>
2018-03-02 22:21:46 +0900

    Check 2nd

--- ch12/list1206.c
+++ ch12/list1206.c
@@ -1,3 +1,4 @@
+// 2nd
 #include <stdio.h>
 
 struct xyz {
@@ -13,7 +14,7 @@ struct xyz xyz_of(int x, long y, double z)
     temp.x = x;
     temp.y = y;
     temp.z = z;
-    
+
     return temp;
 }
 
@@ -21,10 +22,10 @@ int main(void)
 {
     struct xyz s = {0, 0, 0};
 
-    s = xyz_of(12, 1000, 35.689);
+    s = xyz_of(12, 7654321, 35.689);
 
     printf("xyz.x = %d\n", s.x);
-    printf("xyz_y = %ld\n", s.y);
+    printf("xyz.y = %ld\n", s.y);
     printf("xyz.z = %f\n", s.z);
 
     return 0;
